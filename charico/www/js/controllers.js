434052dbf2f35c39372508347830375685721cc7
ehennenfent <ehennenfent@imsa.edu>
2015-09-12 02:53:25 -0400

    Fixed 'Total' method
    Registered a controller that handles the display of the total. Changed
    total function to properly return the result of the API call.

--- charico/www/js/controllers.js
+++ charico/www/js/controllers.js
@@ -21,6 +21,10 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
+.controller('TotalCtrl', function($scope, $http) {
+    $scope.total = Total($scope, $http);
+})
+
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
--- charico/www/js/requests.js
+++ charico/www/js/requests.js
@@ -1,15 +1,15 @@
 function Total($scope, $http) {
-  var total = 0
+  var total = 0;
   $http.get('http://api.reimaginebanking.com/accounts?key=daa76d612ba3bbf96b4a634a3d751ca2')
-  success(function(accounts) {
-    for (account in accounts) {
-      $http.get(concat('http://api.reimaginebanking.com/accounts/', account._id, '/deposits?key=daa76d612ba3bbf96b4a634a3d751ca2'))
-      success(function(deposits) {
-        for (deposit in deposits) {
-          total += deposit.amount
+  .success(function(accounts) {
+    for (var i = 0; i < accounts.length; i++) {
+        var account = accounts[i];
+      $http.get('http://api.reimaginebanking.com/accounts/' + account._id + '/deposits?key=daa76d612ba3bbf96b4a634a3d751ca2').success(function(deposits) {
+        for (var deposit in deposits) {
+          total += deposit.amount;
         }
-      })
+    });
     }
-  })
-  $scope.total = total
+});
+return total;
 }
