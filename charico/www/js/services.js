0a73981a69b012f04032d34f5bf1e5fa88a6e973
JP Smith <jp.smith@ilstu.edu>
2015-09-13 03:38:06 -0400

    charities are in services it's three am oh god what is my life send help

--- charico/www/js/services.js
+++ charico/www/js/services.js
@@ -7,51 +7,51 @@ angular.module('starter.services', [])
   var charities = [{
     id: 0,
     name: 'And Justice For All',
-    description: 'Help provide relief and education for children in developing countries',
-    face: 'https://pbs.twimg.com/profile_images/542228885476081665/MyOfVNsK.png',
-    url: 'http://google.com'
+    description: 'And Justice For All provides legal assistance to the disadvantaged, making sure counsel and assistance are available to those who need it most.',
+    face: 'http://andjusticeforall.org/wp-content/uploads/2012/10/libertylogo.png',
+    url: 'http://andjusticeforall.org/'
   }, {
     id: 1,
     name: 'Midwest Food Bank',
-    description: 'Assist with relief for disaster victims, refugees, and the homeless.',
-    face: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/c4/The_Salvation_Army.svg/868px-The_Salvation_Army.svg.png',
-    url: 'http://google.com'
+    description: 'Midwest Food Bank feeds thousands of the neediest people in the Midwest by operating a network of food banks.',
+    face: 'http://www.midwestfoodbank.org/assets/img/logo.png',
+    url: 'http://midwestfoodbank.org'
   }, {
     id: 2,
     name: 'EFF',
-    description: 'Support cancer research and treatment for current patients.',
-    face: 'http://www.chem.hawaii.edu/Bil301/ACSAstrochemistry/logo1.jpg',
-    url: 'http://google.com'
+    description: 'The EFF fights for the digital and electronic civil rights of people across the world, providing legal services and influencing internet policy.',
+    face: 'https://www.eff.org/files/2014/01/24/eff-logo-plain-300.jpg',
+    url: 'http://eff.org'
   }, {
     id: 3,
     name: 'Venture for America',
-    description: 'Node.js is the only real dev language.',
-    face: 'http://ih1.redbubble.net/image.53589211.4056/fc,550x550,white.u1.jpg',
-    url: 'http://google.com'
+    description: 'Venture for America builds and revitalizes our economy by placing empowering bright and driven students to make an impact through entrepreneurship.',
+    face: 'http://havaspr.com/us/wp-content/uploads/2012/12/Venture-for-America-618-328.jpg',
+    url: 'http://ventureforamerica.org'
   }, {
     id: 4,
     name: 'Water.org',
-    description: 'Help respond to disasters worldwide.',
-    face: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Flag_of_the_ICRC.svg/360px-Flag_of_the_ICRC.svg.png',
-    url: 'http://google.com'
+    description: 'Water.org provides lifesaving water to the neediest people in the world, building clean wells that change communities forever.',
+    face: 'http://water.org/static/image/logo.png?v=1.1',
+    url: 'http://water.org'
     }, {
       id: 5,
       name: 'Doctors Without Borders',
-      description: 'Node.js is the only real dev language.',
-      face: 'http://ih1.redbubble.net/image.53589211.4056/fc,550x550,white.u1.jpg',
-      url: 'http://google.com'
+      description: 'Doctors Without Borders sends doctors across the world to provide life-saving medical care for some of the neediest people in the world.',
+      face: 'http://cdn.doctorswithoutborders.org/sites/all/themes/unified_template/ut_zen/logo.png',
+      url: 'http://doctorswithoutborders.org'
     }, {
       id: 6,
       name: 'Heifer International',
-      description: 'Node.js is the only real dev language.',
-      face: 'http://ih1.redbubble.net/image.53589211.4056/fc,550x550,white.u1.jpg',
-      url: 'http://google.com'
+      description: 'Heifer International empowers the world\'s poorest to change their lifestyle by providing them with a path to economic success through livestock.',
+      face: 'http://www.heifer.org/resources/images/logo.png',
+      url: 'http://heifer.org'
     }, {
       id: 7,
       name: 'Amnesty International',
-      description: 'Node.js is the only real dev language.',
-      face: 'http://ih1.redbubble.net/image.53589211.4056/fc,550x550,white.u1.jpg',
-      url: 'http://google.com'
+      description: 'Amnesty International fights for peace and human rights across the world, championing campaigns to protect the defenseless.',
+      face: 'http://thenewsdoctors.com/wp-content/uploads/2014/09/amnesty-international11.jpg',
+      url: 'http://amnestyusa.org/'
     }];
   if(window.localStorage.getItem("charityOrder") !== null){
         var sorting = JSON.parse(window.localStorage.getItem("charityOrder"));
